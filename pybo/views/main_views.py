5085a3ecf0b2c8cea948ec2efe0ed84767290227
aria9860 <aria9860@naver.com>
2020-09-26 18:06:31 +0900

    정리

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -11,20 +11,9 @@ bp = Blueprint('main', __name__,url_prefix='/')
 def hello_pybo():
     return 'Hello,Pybo'
 
-@bp.route('/coin')
-def index():
-    from .parsing import coinone
-    new_coinone = coinone()
-    BTC_data = new_coinone.BTC
-    ETH_data = new_coinone.ETH
-
-    cat = datetime.now()
-    return render_template('p_example.html', Setprice=BTC_data, secondprice=ETH_data, datenow=cat)
 
 @bp.route('/')
 def loot():
-    return redirect(url_for('question._list'))
+    return render_template('API_loading__example(JS).html')
+    # return redirect(url_for('question._list'))
 
-@bp.route('/test')
-def test():
-    return render_template('question/test_parse.html')
